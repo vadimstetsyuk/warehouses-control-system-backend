190aef542bc125824d6f0df44fb5d9012ded33d8
Вадим Стецюк < >
2017-03-08 17:12:18 +0200

    Added POST request for '/warehouses'

--- routers/warehouses.js
+++ routers/warehouses.js
@@ -9,14 +9,22 @@ warehousesRouter
         console.log('GET /warehouses');
         Warehouse.find((err, warehouses) => {
             if (err) {
-                res.status(400).send(err);
+                res.status(500).send(err);
                 return;
             }
 
             console.log(warehouses);
             res.json(warehouses);
         });
-    });
+    })
+    .post((req, res) => {
+        console.log('POST /warehouses');
 
+        var warehouse = new Warehouse(req.body);
+
+        warehouse.save();
+
+        res.status(201).send(warehouse);
+    });
 
 module.exports = warehousesRouter;
