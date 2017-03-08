4fb784873e60c44e8cfc68f138db9ea6491d77ed
Вадим Стецюк < >
2017-03-08 16:50:41 +0200

    Added GET request for receiving all warehouses

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyBarser = require('body-parser');
+var productsRouter = require('./routers/products');
 var warehousesRouter = require('./routers/warehouses');
 
 var app = express();
@@ -21,4 +22,4 @@ app.use('/api', productsRouter);
 
 app.listen(PORT, () => {
     console.log('Listening on port ' + PORT);
-})
+});
--- routers/warehouses.js
+++ routers/warehouses.js
@@ -1,4 +1,22 @@
 var express = require('express');
-var Warehouses = require('../models/warehouses');
+var Warehouse = require('../models/warehouse');
 
 var warehousesRouter = express.Router();
+
+warehousesRouter
+    .route('/warehouses')
+    .get((req, res) => {
+        console.log('GET /warehouses');
+        Warehouse.find((err, warehouses) => {
+            if (err) {
+                res.status(400).send(err);
+                return;
+            }
+
+            console.log(warehouses);
+            res.json(warehouses);
+        });
+    });
+
+
+module.exports = warehousesRouter;
