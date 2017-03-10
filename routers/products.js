0923d78835559e7a65d50812e16dc3cdc4caea62
Вадим Стецюк < >
2017-03-10 13:39:04 +0200

    Added GET & POST request for '/products'

--- routers/products.js
+++ routers/products.js
@@ -3,4 +3,28 @@ var Product = require('../models/product');
 
 var productsRouter = express.Router();
 
+productsRouter
+    .route('/products')
+    .get((req, res) => {
+        console.log('GET /products');
+        Product.find((err, products) => {
+            if (err) {
+                res.status(500).send(err);
+                return;
+            }
+
+            console.log(products);
+            res.json(products);
+        });
+    })
+    .post((req, res) => {
+        console.log('POST /products');
+
+        var product = new Product(req.body);
+
+        product.save();
+
+        res.status(201).send(product);
+    });
+
 module.exports = productsRouter;
