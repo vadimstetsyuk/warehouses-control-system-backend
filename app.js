5ab25b08db01a2b327f4b1f655aa345dfd6d8715
Вадим Стецюк < >
2017-03-12 17:38:45 +0200

    Enabled support CORS on the server

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ var usersRouter = require('./routers/users');
 
 var app = express();
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 var PORT = 3000;
 var HOST_NAME = 'localhost';
 var DATABASE_NAME = 'warehousesdb';
